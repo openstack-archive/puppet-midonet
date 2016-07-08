f329587b284dffbc64c992017f36ee8b85f6b712
Alejandro Andreu <alejandro@midokura.com>
2016-07-08 16:28:09 +0200

    Adds unit tests for midonet::repository classes
    Change-Id: Iac3d4dde6cdc52bf8b16a8421ff7a23bb84a8320

---
+++ spec/classes/midonet_repository_centos_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+
+describe 'midonet::repository::centos' do
+  context 'with default parameters (centos)' do
+    let :facts do
+      {
+        :osfamily                     => 'RedHat',
+        :operatingsystemmajrelease    => '7'
+      }
+    end
+    let :params do
+      {
+        :is_mem => false,
+        :midonet_version => '5.2',
+        :midonet_stage => 'stable',
+        :openstack_release => 'mitaka',
+        :mem_version => '5',
+        :mem_username => nil,
+        :mem_password => nil,
+      }
+    end
+
+    it { is_expected.to contain_class('midonet::params') }
+
+    it { is_expected.to contain_yumrepo('midonet').with(
+        'name' => 'midonet',
+        'baseurl' => 'http://builds.midonet.org/midonet-5.2/stable/el7',
+        'enabled' => '1',
+        'gpgcheck' => '1',
+        'gpgkey' => 'https://builds.midonet.org/midorepo.key',
+        'timeout' => '60'
+      )
+    }
+    it { is_expected.to contain_yumrepo('midonet-openstack-integration').with(
+        'name' => 'midonet-openstack-integration',
+        'baseurl' => 'http://builds.midonet.org/openstack-mitaka/stable/el7',
+        'enabled' => '1',
+        'gpgcheck' => '1',
+        'gpgkey' => 'https://builds.midonet.org/midorepo.key',
+        'timeout' => '60'
+      )
+    }
+  end
+
+  context 'with custom parameters (centos)' do
+    let :facts do
+      {
+        :osfamily                     => 'RedHat',
+        :operatingsystemmajrelease    => '7'
+      }
+    end
+    let :params do
+      {
+        :is_mem => true,
+        :midonet_version => '5.2',
+        :midonet_stage => 'stable',
+        :openstack_release => 'mitaka',
+        :mem_version => '6',
+        :mem_username => 'sample_username',
+        :mem_password => 'sample_password',
+      }
+    end
+
+    it { is_expected.to contain_class('midonet::params') }
+
+    it { is_expected.to contain_yumrepo('midonet').with(
+        'name' => 'mem',
+        'baseurl' => 'http://sample_username:sample_password@builds.midonet.org/mem-6/stable/el7',
+        'enabled' => '1',
+        'gpgcheck' => '1',
+        'gpgkey' => 'https://builds.midonet.org/midorepo.key',
+        'timeout' => '60'
+      )
+    }
+    it { is_expected.to contain_yumrepo('midonet-openstack-integration').with(
+        'name' => 'mem-openstack-integration',
+        'baseurl' => 'http://builds.midonet.org/openstack-mitaka/stable/el7',
+        'enabled' => '1',
+        'gpgcheck' => '1',
+        'gpgkey' => 'https://builds.midonet.org/midorepo.key',
+        'timeout' => '60'
+      )
+    }
+  end
+end
---
+++ spec/classes/midonet_repository_ubuntu_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe 'midonet::repository::ubuntu' do
+  context 'with default parameters (debian)' do
+    let :facts do
+      {
+        :osfamily           => 'Debian',
+        :lsbdistid          => 'Ubuntu',
+        :lsbdistrelease     => '16.04',
+      }
+    end
+    let :params do
+      {
+        :is_mem => false,
+        :midonet_version => '5.2',
+        :midonet_stage => 'stable',
+        :openstack_release => 'mitaka',
+        :mem_version => '5',
+        :mem_username => nil,
+        :mem_password => nil,
+      }
+    end
+
+    it { is_expected.to contain_class('midonet::params') }
+    it { is_expected.to contain_class('apt') }
+    it { is_expected.to contain_class('apt::update') }
+    it { is_expected.to contain_apt__key('midorepo') }
+
+    it { is_expected.to contain_apt__source('midonet').with(
+        'location' => 'http://builds.midonet.org/midonet-5.2',
+        'release' => 'stable',
+        'key' => '{"id"=>"E9996503AEB005066261D3F38DDA494E99143E75", "server"=>"subkeys.pgp.net"}',
+        'include' => '{"src"=>false}'
+      )
+    }
+    it { is_expected.to contain_apt__source('midonet-openstack-integration').with(
+        'location' => 'http://builds.midonet.org/openstack-mitaka',
+        'release' => 'stable',
+        'include_src' => 'false'
+      )
+    }
+    it { is_expected.to contain_exec('update-midonet-repos').with(
+        'command' => '/bin/true',
+        'require' => '[Exec[apt_update]{:command=>"apt_update"}, Apt::Source[midonet]{:name=>"midonet"}, Apt::Source[midonet-openstack-integration]{:name=>"midonet-openstack-integration"}]'
+      )
+    }
+  end
+
+  context 'with custom parameters (debian)' do
+    let :facts do
+      {
+        :osfamily           => 'Debian',
+        :lsbdistid          => 'Ubuntu',
+        :lsbdistrelease     => '16.04',
+      }
+    end
+    let :params do
+      {
+        :is_mem => true,
+        :midonet_version => '5.2',
+        :midonet_stage => 'stable',
+        :openstack_release => 'mitaka',
+        :mem_version => '6',
+        :mem_username => 'sample_username',
+        :mem_password => 'sample_password',
+      }
+    end
+
+    it { is_expected.to contain_class('midonet::params') }
+    it { is_expected.to contain_class('apt') }
+    it { is_expected.to contain_class('apt::update') }
+    it { is_expected.to contain_apt__key('midorepo') }
+
+    it { is_expected.to contain_apt__source('midonet').with(
+        'location' => 'http://sample_username:sample_password@builds.midonet.org/mem-6',
+        'release' => 'stable',
+        'key' => '{"id"=>"E9996503AEB005066261D3F38DDA494E99143E75", "server"=>"subkeys.pgp.net"}',
+        'include' => '{"src"=>false}'
+      )
+    }
+    it { is_expected.to contain_apt__source('midonet-openstack-integration').with(
+        'location' => 'http://builds.midonet.org/openstack-mitaka',
+        'release' => 'stable',
+        'include_src' => 'false'
+      )
+    }
+    it { is_expected.to contain_exec('update-midonet-repos').with(
+        'command' => '/bin/true',
+        'require' => '[Exec[apt_update]{:command=>"apt_update"}, Apt::Source[midonet]{:name=>"midonet"}, Apt::Source[midonet-openstack-integration]{:name=>"midonet-openstack-integration"}]'
+      )
+    }
+  end
+end
