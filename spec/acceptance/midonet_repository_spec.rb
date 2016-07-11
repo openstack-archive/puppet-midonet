6b8462adbadeceb22d623b3ec7a484263a780a78
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-11 14:44:55 +0200

    Add Testing for MEM/no-mem and misc repo in repository class testing
    Change-Id: I8fff6578821d33731579a5df1cc82fea1e39c7af

--- spec/acceptance/midonet_repository_spec.rb
+++ spec/acceptance/midonet_repository_spec.rb
@@ -44,4 +44,64 @@ describe 'midonet class' do
     end
 
   end
+
+
+## WITH MEM
+
+context 'WITH mem' do
+   # Using puppet_apply as a helper
+   it 'should work without any errors' do
+     pp = <<-EOS
+       class {'::midonet::repository':
+         is_mem => true
+       }
+     EOS
+
+     # Run it twice and test for idempotency
+     expect(apply_manifest(pp).exit_code).to_not eq(1)
+     expect(apply_manifest(pp).exit_code).to eq(0)
+   end
+
+
+
+   # **************************************************************************
+   # CHECK REPOSITORIES CONFIGURED PROPERLY
+   # **************************************************************************
+   $apt_repository_files = [ '/etc/apt/sources.list.d/mem.list',
+                             '/etc/apt/sources.list.d/mem-openstack-integration.list',
+                             '/etc/apt/sources.list.d/mem-misc.list',
+                           ]
+   # Midonet
+   if os[:family] == 'redhat'
+
+     describe yumrepo('mem') do
+       it { should exist }
+       it { should be_enabled }
+     end
+
+     describe yumrepo('mem-openstack-integration') do
+       it { should exist }
+       it { should be_enabled }
+     end
+
+     describe yumrepo('mem-misc') do
+       it { should exist }
+       it { should be_enabled }
+     end
+   end
+
+   # Midonet
+   if os[:family] == 'Ubuntu'
+     $apt_repository_files.each do |apt_repository_file|
+       describe file(apt_repository_file) do
+         it { is_expected.to exist }
+         it { is_expected.to contain('midokura') }
+       end
+     end
+   end
+
+ end
+
+
+
 end
