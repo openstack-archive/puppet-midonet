0920422600adc7c742e771d451bf768347b0368f
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-18 14:29:07 +0200

    Add Midonet Cluster Acceptance
    Change-Id: I2cf525f93f08cefcac81e27cdf45ffc59bd18c00

---
+++ spec/acceptance/midonet_cluster_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper_acceptance'
+
+describe 'midonet::cluster class' do
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should install the midonet cluster without any errors' do
+      pp = <<-EOS
+      class { 'midonet::cluster':
+        zookeeper_hosts       => [{ 'ip' => '127.0.0.1', 'port' => '2181' }],
+        cassandra_servers     => ['127.0.0.1'],
+        cassandra_rep_factor  => 1,
+        keystone_admin_token  => '',
+        keystone_host         => '127.0.0.1'
+       }
+      EOS
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe package('midonet-cluster') do
+      it { should be_installed }
+      it { should be_running }
+    end
+
+    # JMX
+    describe port(8181) do
+      it { should be_listening }
+    end
+  end
+
+  context
+
+end
