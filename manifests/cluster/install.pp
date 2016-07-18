ac7fbc9b8eb0af3a3dcc062b58d8a38ce707cff6
Alejandro Andreu <alejandro@midokura.com>
2016-07-18 06:52:56 +0200

    Initial commit for MN Cluster
    Change-Id: Ic49335035a443d6a21a9575327c769c626e5bbe9

---
+++ manifests/cluster/install.pp
@@ -0,0 +1,35 @@
+# == Class: midonet::midonet_api::install
+# Check out the midonet::midonet_api class for a full understanding of
+# how to use the midonet_api resource
+#
+# === Authors
+#
+# Midonet (http://midonet.org)
+#
+# === Copyright
+#
+# Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+class midonet::cluster::install (
+  $package_name      = 'midonet-cluster',
+) {
+
+  include midonet::repository
+
+  package {'midonet-cluster':
+    ensure => present,
+    name   => $package_name,
+  }
+}
