9c0279dcb3440665f854b1f9c0306294c926719b
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-26 18:22:05 +0200

    Add Old fashioned to new CentOs7 interface name conversion function
    Change-Id: I9326dbad4d05521913c2a3769679a93ec56db6e4

---
+++ lib/puppet/parser/functions/c7_int_name.rb
@@ -0,0 +1,53 @@
+require 'facter'
+
+# Function to figure out old school to new school mappings
+# Uses the biosdevname info to generate a mapping
+module Puppet::Parser::Functions
+  newfunction(:c7_int_name, :type => :rvalue) do |args|
+
+    if args.length != 1
+      raise(Puppet::ParseError, "No interface name passed to convert")
+    end
+
+    int_name = args[0]
+
+    if Facter.value('osfamily') == 'Debian'
+      #nothing to do here..
+      return int_name
+    end
+
+    # Don't need any of this logic for 6
+    Facter.loadfacts()
+    os_majrelease = Facter.value('operatingsystemmajrelease')
+    if os_majrelease != '7'
+      return int_name
+    end
+
+    # Get the kernel names
+    interfaces = {}
+    kernel_devs=`/usr/sbin/biosdevname -d`
+    kernel_devs.split("\n").each do |dev_info|
+      # Split the values
+      name, value = dev_info.split(':', 2)
+
+      # Stip the whitespace out
+      name = name.sub(/^[\s\n\r]*/, '').sub(/[\s\n\r]*$/, '')
+      value = value.sub(/^[\s\n\r]*/, '').sub(/[\s\n\r]*$/, '')
+
+      # Grab the kernel names
+      if name == 'Kernel name'
+        # Figure out the old school mapping
+        old_school=`/usr/sbin/biosdevname --policy=all_ethN -i '#{value}'`
+        interfaces[old_school.chomp] = value
+      end
+    end
+
+    # Lookup
+    if interfaces.has_key?(int_name)
+      return interfaces[int_name]
+    end
+
+    # Default to what was passed
+    return int_name
+  end
+end
