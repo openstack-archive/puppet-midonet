cba439156d4b0315d6304172b2967a16ec780d83
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-08-02 16:34:15 +0200

    Add midonet-cluster-mem if its mem install
    Change-Id: I930b03f238d1ac6c4188e910a808989b11fda980

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -34,6 +34,20 @@
 #   Keystone host
 # [*keystone_port*]
 #   Keystone port
+# [*max_heap_size*]
+#   Java Max Heap Size
+# [*heap_newsize*]
+#   Java heap size default Size
+# [*is_mem*]
+#   Using MEM installation?
+# [*heap_newsize*]
+#   Java heap size default Size
+# [*manage_repos*]
+#   should manage repositories?
+# [*mem_username*]
+#   Midonet MEM username
+# [*mem_password*]
+#   Midonet MEM password
 #
 # === Examples
 #
@@ -86,11 +100,16 @@ class midonet::cluster (
   $keystone_port           = undef,
   $max_heap_size           = undef,
   $heap_newsize            = undef,
+  $is_mem                  = false,
+  $manage_repo             = false,
+  $mem_username            = undef,
+  $mem_password            = undef,
   $zookeeper_hosts,
   $cassandra_servers,
   $cassandra_rep_factor,
   $keystone_admin_token,
   $keystone_host,
+
 ) {
 
     class { 'midonet::cluster::install':
@@ -113,5 +132,29 @@ class midonet::cluster (
       keystone_admin_token    => $keystone_admin_token,
       keystone_host           => $keystone_host,
       keystone_port           => $keystone_port
+    }
+
+    if $is_mem {
+      if $manage_repo == true {
+        if !defined(Class['midonet::repository']) {
+          class {'midonet::repository':
+            is_mem            => $is_mem,
+            midonet_version   => undef,
+            midonet_stage     => undef,
+            openstack_release => undef,
+            mem_version       => undef,
+            mem_username      => $mem_username,
+            mem_password      => $mem_password
+          }
+        }
+      }
+      package { 'midonet-cluster-mem':
+        ensure  => present
+        require => [Class['midonet::repository'],
+                    Class['midonet::cluster::run'],
+                    Class['midonet::cluster::install']]}
+    }
+    else  {
+      notice('Skipping installation of midonet-cluster-mem')
     }
 }
