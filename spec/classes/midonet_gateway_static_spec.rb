674ad98cf9cd42debb68cad48b51122986cc16a1
Alejandro Andreu <alejandro@midokura.com>
2016-08-12 11:04:50 +0200

    Tweaks create_fake_uplink_l2.sh script
    Change-Id: I64389c86525a07673df1e2b227b1af180b15df4d

--- spec/classes/midonet_gateway_static_spec.rb
+++ spec/classes/midonet_gateway_static_spec.rb
@@ -11,19 +11,18 @@ describe 'midonet::gateway::static' do
     end
     let :params do
       {
-        :network_id => 'example_netid',
-        :cidr => '200.0.0.1/24',
-        :gateway_ip => '200.0.0.1',
-        :service_host => '127.0.0.1',
-        :service_dir => '/tmp/status',
-        :zookeeper_hosts => [ { 'ip' => '127.0.0.1', 'port' => '2181' } ],
-        :api_port => '8181'
+      :nic            => 'enp0s3',
+      :fip            => '200.200.200.0/24',
+      :edge_router    => 'edge-router',
+      :veth0_ip       => '172.19.0.1',
+      :veth1_ip       => '172.19.0.2',                                                                                                                                                                                                     
+      :veth_network   => '172.19.0.0/30',
+      :scripts_dir    => '/tmp',
+      :uplink_script  => 'create_fake_uplink_l2.sh',
+      :ensure_scripts =>  'present',
       }
     end
-    it { is_expected.to contain_package('screen').with_ensure('installed') }
     it { is_expected.to contain_file('fake_uplink_script').with_ensure('present') }
-    it { is_expected.to contain_file('midorc').with_ensure('present') }
-    it { is_expected.to contain_file('functions').with_ensure('present') }
     it { is_expected.to contain_exec('/bin/bash /tmp/create_fake_uplink_l2.sh') }
   end
 end
